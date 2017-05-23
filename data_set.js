c256bd0c28794be63f41f1ee04cb3b92a9017aee
JongHyeok Choi <asdfg9822@naver.com>
2017-05-23 23:07:34 +0900

    Module화 하기

---
+++ data_set.js
@@ -0,0 +1,19 @@
+/**
+ * Created by JongHyeok Choi on 2017. 5. 23..
+ */
+
+// module.exports = {
+//     getAnalyzedData: function () {
+//
+//     }
+// };
+//
+//
+//
+// //Analysis Polymer Element
+// hyd.Analyzer.analyze(path)
+//     .then(function(analyzer) {
+//         var eleAnalyzed = analyzer.elementsByTagName[fileNms[fileNms.length - 1]];
+//
+//
+//     });
--- main.js
+++ main.js
@@ -2,9 +2,56 @@
  * Created by user on 2017. 5. 23..
  */
 
-var hyd = require('hydrolysis');
+/**
+ * Module
+ */
+var fs = require('fs');
+var _ = require('underscore');
 
-hyd.Analyzer.analyze('./input/test.html')
-    .then(function(analyzer) {
-        console.log(analyzer.elementsByTagName['test']);
+/**
+ * Custom Module
+ */
+var DS = require('data_set');
+
+/**
+ * Global Variable
+ */
+var inputDir = './input';
+var outputDir = './output';
+
+/**
+ * Recursive Search
+ */
+(function searchDir(dir) {
+    fs.readdir(dir, function (err, files) {
+        files.forEach(function (file) {
+            var path = [dir,file].join('/');
+            fs.stat(path, function (err, stats) {
+                (stats.isDirectory()) ? searchDir(path): fileProcess(path);
+            })
+        });
     });
+
+    //File Process
+    function fileProcess(path) {
+        var paths = path.split('.');
+        var pathsLen = paths.length;
+
+        //HTML Validation
+        if(paths[pathsLen - 1] !== 'html') {
+            console.warn(path, "- This file is not a HTML file");
+            return;
+        }
+
+        var fileNms = paths[pathsLen - 2].split('/');
+
+        //Analysis Polymer Element
+        hyd.Analyzer.analyze(path)
+            .then(function(analyzer) {
+                var eleAnalyzed = analyzer.elementsByTagName[fileNms[fileNms.length - 1]];
+
+
+            });
+    }
+})(inputDir);
+
