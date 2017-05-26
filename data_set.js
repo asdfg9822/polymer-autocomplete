1e7c97c3cec06d8a53aa7eb13ef0b02af8301798
JongHyeok Choi <asdfg9822@naver.com>
2017-05-26 18:05:53 +0900

    - Mode 분리  - Editor별 Config 관리  -

--- data_set.js
+++ data_set.js
@@ -13,7 +13,7 @@ module.exports = (path) => {
     return analyze(path);
 };
 
-let analyze = (path)=> {
+let analyze = (path) => {
     //Analysis Polymer Element
     hyd.Analyzer.analyze(path)
         .then(settingData);
@@ -21,37 +21,115 @@ let analyze = (path)=> {
 
 let parser = new xml2js.Parser();
 let settingData = (analyzer) => {
-let result = {
-    template: {}
-};
+    let result = {
+        template: [] // Template 태그는 여러 개의 태그를 담는다
+    };
 
-    analyzer.elements.forEach((element) => {
-        fs.readFile('./format_example/Base.xml', function(err, data) {
-            //parser.parseString(data, {'newline': '\n'}, (err, tmp) => {
-                let $ = result.template["$"] = {};
 
-                fnExcute(webstormHandler.is, $, element.is);
-                fnExcute(webstormHandler.desc, $, "test");
+    //기본 템플릿 불러오기
+    fs.readFile('./format_example/Base.xml', function (err, data) {
+        parser.parseString(data, (err, tmp) => {
 
+            //Element별로 처리
+            analyzer.elements.forEach((element) => {
+                let eleObj = {};
+
+                fnExcute(webstormHandler.is, eleObj, element.is);
+                fnExcute(webstormHandler.desc, eleObj, "test");
+
+                element.properties.forEach((p) => {
+                    //Properties Type이 getter 또는 Function이 아닐 때
+                    if (p.type === "" || p.type === 'Function') {
+
+                    } else if (snippetConfig.mode === MODE.ALL) {
+                        console.log(p.name, p.type);
+
+
+
+                    } else if (snippetConfig.mode === MODE.EXIST_ANNOTATION) {
+
+                    } else if (snippetConfig.mode === MODE.EXIST_DESC) {
+
+                    } else if (snippetConfig.mode === MODE.CUSTOM_ANNOTATION) {
+
+                    }
+                });
+
+                //Push Data
+                result.template.push({template: eleObj});
+
+                //JSON to XML
                 var builder = new xml2js.Builder();
                 var xml = builder.buildObject(result);
 
-                console.log(xml);
+                //Additory Working for Output File
+                //Get TemplateSet
 
+                //Write File
                 fs.writeFile('./output/Result.xml', xml, (err) => {
                     if (err) throw err;
                     console.log('The file has been saved!');
                 });
-            //});
+            });
         });
     });
 }
 
+let snippetConfig = {
+    get mode() {
+        return MODE.ALL;
+    },
+};
+
+let MODE = {
+    //공통적으로 Function, Getter 제외
+
+    ALL: 1, //모든 Property에 대해
+    EXIST_ANNOTATION: 2, //Annotation이 있는 태그에 대해
+    EXIST_DESC: 3,  //Comment가 존재하는 경우
+    CUSTOM_ANNOTATION: 4 //특정 어노테이션이 있는 경우
+};
+
+//fn이 함수이면 결과 값 반환 아니면 undefined
 let fnExcute = (fn, ...args) => {
-    return _.isFunction(fn) ? fn.apply(webstormHandler, args) : fn;
+    if (_.isFunction(fn)) {
+        return fn.apply(webstormHandler, args);
+    }
+    return undefined;
 }
 
+//Webstrom Code Snippet Config Handler
 let webstormHandler = {
+    is: (tmp, value) => {
+        tmp.name = value;
+    },
+    desc: (tmp, value) => {
+        tmp.description = value;
+    }
+}
+
+//Atom Code Snippet Config Handler
+let atomHandler = {
+    is: (tmp, value) => {
+        tmp.name = value;
+    },
+    desc: (tmp, value) => {
+        tmp.description = value;
+    }
+}
+
+//Sublime Code Snippet Config Handler
+let sublimeHandler = {
+    is: (tmp, value) => {
+        tmp.name = value;
+    },
+    desc: (tmp, value) => {
+        tmp.description = value;
+    }
+}
+
+//VS Code Snippet Config Handler
+let vscodeHandler = {
     is: (tmp, value) => {
         tmp.name = value;
     },
